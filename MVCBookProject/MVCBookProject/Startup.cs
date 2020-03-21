7a16cdedb8f8bfad80530240d48377ef3662fee6
Priyaben Desai <P2412923@my365.dmu.ac.uk>
2020-03-21 19:06:42 +0000

    created mvcbookproject

---
+++ MVCBookProject/MVCBookProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCBookProject.Startup))]
+namespace MVCBookProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

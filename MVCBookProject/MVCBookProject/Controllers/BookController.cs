2414db9f2df1b1cdbed2921817958e1a0273a935
Priyaben Desai <P2412923@my365.dmu.ac.uk>
2020-03-22 20:59:05 +0000

    Continuing with the BenSimmons page

---
+++ MVCBookProject/MVCBookProject/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCBookProject.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult BenSimmons()
+        {
+            
+            
+                ViewBag.Title = "Ben Simmons";
+                ViewBag.Author = "Ben Simmons";
+                ViewBag.Content = "This book is written by Ben Simmons";
+            
+            return View();
+        }
+    }
+}
--- MVCBookProject/MVCBookProject/Models/BookModel.cs
+++ MVCBookProject/MVCBookProject/Models/BookModel.cs
@@ -7,5 +7,8 @@ namespace MVCBookProject.Models
 {
     public class BookModel
     {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
     }
 }

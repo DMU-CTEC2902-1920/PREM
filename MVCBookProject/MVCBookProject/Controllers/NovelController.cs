bab4c913c39d0ded4feb24a0fb312401d794bccc
Michael babz <michaelbabalola79@gmail.com>
2020-03-24 22:52:45 +0000

    priya made pages views conttrollers eddie did info regarding ooks with help of babz

---
+++ MVCBookProject/MVCBookProject/Controllers/NovelController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBookProject.Controllers
+{
+    public class NovelController : Controller
+    {
+        // GET: Novel
+        public ActionResult BenSimmons()
+        {
+            ViewBag.Message = "Ben simmons";
+            return View();
+        }
+
+        public ActionResult Legends()
+        {
+            ViewBag.Message = "Legends";
+            return View();
+        }
+
+        public ActionResult MambaMentality()
+        {
+            ViewBag.Message = "MambaMentality";
+            return View();
+        }
+    }
+
+
+}

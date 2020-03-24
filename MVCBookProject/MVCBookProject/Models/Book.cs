9aa4f432f3613e0832e6e1221b3f3c73ff1defd5
Michael babz <michaelbabalola79@gmail.com>
2020-03-24 20:33:45 +0000

    made add book plus data layer

--- MVCBookProject/MVCBookProject/Models/Book.cs
+++ MVCBookProject/MVCBookProject/Models/Book.cs
@@ -7,18 +7,12 @@ namespace MVCBookProject.Models
 {
     public class Book
     {
-        public virtual int ISBN { get; set; }
-        public virtual int GenreId{ get; set; }
+        public virtual int BookId { get; set; }
+        public virtual int GenreId { get; set; }
         public virtual string Author { get; set; }
         public virtual string Title { get; set; }
         public virtual string Publisher { get; set; }
         public virtual DateTime YearPublished { get; set; }
-        public virtual int ShelfNumber { get; set; }
-        public virtual int EditionNumber { get; set; }
-        public virtual string GenreName { get; set; }
-
-
-
-
+        public virtual Genre Genre { get; set; }
     }
 }
--- MVCBookProject/MVCBookProject/Models/Genre.cs
+++ MVCBookProject/MVCBookProject/Models/Genre.cs
@@ -8,7 +8,7 @@ namespace MVCBookProject.Models
     public class Genre
     {
         public virtual int GenreId { get; set; }
-        public virtual string GenreName { get; set; }
-        
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
     }
 }
